0dd62f7a73436cc4f84906d056621d9e856f299f
anizami <anizami@macalester.edu>
2014-04-23 19:32:49 -0500

    Added all PiperText class

---
+++ src/main/java/PiperText.java
@@ -0,0 +1,26 @@
+/**
+ * Created by anizami on 4/23/14.
+ */
+public class PiperText {
+
+    private String title = null;
+    private String body = null;
+
+
+    public void setTitle (String input){
+        this.title = input;
+    }
+    public void setBody (String input){
+        this.body = input;
+    }
+    public String getTitle (){
+        return title;
+    }
+    public String getBody (){
+        return body;
+    }
+
+    public PiperText () {
+
+    }
+}
