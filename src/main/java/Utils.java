5f1bacbb5673d0bf9101b8ab8f508de0dbc7e567
Asra Nizami <anizami@macalester.edu>
2014-05-06 12:50:22 -0500

    updated server

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -12,7 +12,6 @@ public class Utils {
 
     static String lookBehind = "(?<=(^| ))";
     static String lookAhead = "(?=($| |\\.|\\!|\\?|\\,|\\;|\\:))";
-
     static String foodRegex = lookBehind + "([Bb]reakfast|[Ll]unch|[Dd]inner|[Ss]nack[s]|[Bb]agels|[Rr]efreshments|[Pp]ie|[Cc](ake|offee|hocolate|andy)|[Ii]ce cream]|[Tt]ea|[Dd]rinks|[Pp]izza])" + lookAhead;
     static String placeRegex = lookBehind + "([Oo]lin[ -][Rr]ice|[Cc]ampus[ -][Cc]enter|[Cc]arnegie|[Oo]ld [Mm]ain|[Hh]all [Oo]f [Ff]ame [Rr]oom|[Hh]armon [Rr]oom|[Ff]rench [Mm]eadow [Bb]akery|(Russian|Japan|French|German|Spanish|Chinese) [Hh]ouse|[Mm]arkim [Hh]all|[Ss]tudent [Gg]allery|Koch [Ll]ounge|[Ww]eyerhaeuser ([Mm]emorial)? [Cc]hapel|[Kk]agin|GSRC|[Kk]irk|[Dd]upre|[Tt]urck|[Dd]oty|GDD|G.D.D.|[M]innehaha [Ff]alls|[Bb]igelow|[Ww]eyerhaeuser ([Bb]oardroom)?|[Ss]cience [Mm]useum [Oo]f [Mm]innesota|[Dd]eWitt [Ww]allace [Ll]ibrary)" + lookAhead;
     static String timeRegex = lookBehind + "([0-9]{1,2}(:[0-9][0-9])?(( a.m.)|( p.m.))?|(noon))(( to )|( ?- ?))([0-9]{1,2}(:[0-9][0-9])?(( a.m.)|( p.m.))?|(noon))|([0-9]{1,2}:[0-9]{2,2}( a.m.| p.m.)?)|([0-9]{1,2}( a.m.| p.m.)|noon)" + lookAhead;
